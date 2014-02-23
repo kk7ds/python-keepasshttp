4014ed5ff59b04d4fce284fa8a28f4c6bd646eb7
Dan Smith <dsmith@danplanet.com>
2014-02-23 07:59:51 -0800

    Import cleanup and copyright/license statements

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import sys
+# Copyright 2014 Dan Smith <dsmith@danplanet.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from StringIO import StringIO
-import json
-import base64
+from ConfigParser import ConfigParser
 from Crypto.Cipher import AES
 from Crypto import Random
-from ConfigParser import ConfigParser
-import uuid
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from StringIO import StringIO
+import base64
+import json
 from optparse import OptionParser
 import subprocess
+import sys
+import uuid
 
 import keepass_util
 
